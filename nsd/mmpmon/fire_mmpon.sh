383120510afb7c09ab5d1aaeb186ae70bddb10ae
malone12 <malone12@illinois.edu>
2021-10-15 20:35:40 -0500

    Removed date tag from performance metrics to greatly prevent cardinality expansion over time

--- nsd/mmpmon/fire_mmpon.sh
+++ nsd/mmpmon/fire_mmpon.sh
@@ -5,12 +5,10 @@ cmgr=$(sudo /usr/lpp/mmfs/bin/mmlsmgr -c | cut -d'(' -f 2 | cut -d')' -f 1)
 
 if [[ "$cmgr" == *"$myid"* ]]; then
 
-day=$(date +%Y_%m_%d)
-
-sudo /usr/lpp/mmfs/bin/mmpmon -i /etc/telegraf/mmpmon/infile_clients -p | awk -v day="$day" '!/_rc_ 0/ {next;} /fs_io_s/ {print "gpfsperf,fs="$15",day="day",node="$5" br="$19",bw="$21",oc="$23",cc="$25",rr="$27",wr="$29}'
+sudo /usr/lpp/mmfs/bin/mmpmon -i /etc/telegraf/mmpmon/infile_clients -p | awk '!/_rc_ 0/ {next;} /fs_io_s/ {print "gpfsperf,fs="$15",node="$5" br="$19",bw="$21",oc="$23",cc="$25",rr="$27",wr="$29}'
 
 sudo /usr/lpp/mmfs/bin/mmpmon -i /etc/telegraf/mmpmon/infile_nsd -p > /tmp/nsd_perf.out
-awk -v day="$day" '
+awk '
 # read in mapfile
 NR==FNR { nsds[$2]=$1; next; }
 # skip lines with RC==1
@@ -30,7 +28,7 @@ NR==FNR { nsds[$2]=$1; next; }
          wbytes=$5;
       }
    }
-   printf "gpfsnsdperf,fs=%s,day=%s,node=%s,nsd=%s rops=%s,wops=%s,rbytes=%s,wbytes=%s\n",nsds[nsd],day,node,nsd,rops,wops,rbytes,wbytes;
+   printf "gpfsnsdperf,fs=%s,node=%s,nsd=%s rops=%s,wops=%s,rbytes=%s,wbytes=%s\n",nsds[nsd],node,nsd,rops,wops,rbytes,wbytes;
 }' /etc/telegraf/mmpmon/nsd_map /tmp/nsd_perf.out
 
 rm -rf /tmp/nsd_perf.out
